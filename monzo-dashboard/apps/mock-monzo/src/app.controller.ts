0801644973278680153ae03525d60728825d42bb
Jack Francis Dalton <jackfrancisdalton@gmail.com>
2025-07-03 15:26:37 +0100

    chore: minor clean up and stubbing of mock-monzo controller and service

--- monzo-dashboard/apps/mock-monzo/src/app.controller.ts
+++ monzo-dashboard/apps/mock-monzo/src/app.controller.ts
@@ -6,7 +6,18 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getAccounts(): any {
+    return {}
   }
+
+  @Get('/balance')
+  getBalance() {
+    return {}
+  }
+
+  @Get()
+  getTransactions(): any {
+    return {}
+  }
+
 }
