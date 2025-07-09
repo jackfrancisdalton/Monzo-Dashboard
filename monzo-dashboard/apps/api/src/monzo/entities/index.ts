c540c724a92bbd6ed9af96d1a87f8fefe6dbb600
Jack Francis Dalton <jackfrancisdalton@gmail.com>
2025-07-09 19:52:21 +0100

    feat: added monzo sync service for handling initial full load, and incremental sync of data from monzo to sql db

---
+++ monzo-dashboard/apps/api/src/monzo/entities/index.ts
@@ -0,0 +1,5 @@
+export * from './account.entity';
+export * from './balance.entity';
+export * from './transaction.entity';
+export * from './merchant.entity';
+export * from './merchant-address.entity';
