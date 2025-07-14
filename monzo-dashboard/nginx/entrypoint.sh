796b1dacf641bb6337da77358120e4a7257d0591
Jack Francis Dalton <jackfrancisdalton@gmail.com>
2025-07-14 07:54:13 +0100

    feat: made reverse proxy port controlable with a single env var in case users already have 80 bound

--- monzo-dashboard/nginx/entrypoint.sh
+++ monzo-dashboard/nginx/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 echo "Rendering nginx.conf from template with env vars..."
-envsubst '${FRONTEND_DOCKER_URL} ${API_DOCKER_URL} ${MOCK_MONZO_DOCKER_URL}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '${NGINX_PORT} ${FRONTEND_DOCKER_URL} ${API_DOCKER_URL} ${MOCK_MONZO_DOCKER_URL}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 exec nginx -g 'daemon off;'
